672a9162dc7fe33c30eddb8b7191afc384e00a8a
umu <46300268+xZetsubou@users.noreply.github.com>
2023-08-31 02:04:58 +0300

    Pull Sub Devices if we can't find gateway.
    We stop the pulling process if gateway doesn't exists which is rare to happen, But if gateway doesn't exists we will pull the device without Gateway IP.

--- custom_components/localtuya/config_flow.py
+++ custom_components/localtuya/config_flow.py
@@ -189,12 +189,15 @@ def mergeDevicesList(devList: dict, cloudList: dict, addSubDevices=True) -> dict
                     newList[f"Sub Device"] = _devID
                 else:
                     # Create a data for sub_device [cloud and local gateway] to merge it with discovered devices.
-                    local_GW = devList[gateway[0].get(CONF_ID)]
+                    if len(gateway) > 0:
+                        local_GW = devList[gateway[0].get(CONF_ID)]
+                    else:  # If we can't find the gateway we will pull data without local "ip"
+                        local_GW = cloudList[_devID]
                     dev_data = {
                         _devID: {
                             CONF_TUYA_IP: local_GW.get(CONF_TUYA_IP),
                             CONF_TUYA_GWID: _devID,
-                            CONF_TUYA_VERSION: local_GW.get(CONF_TUYA_VERSION),
+                            CONF_TUYA_VERSION: local_GW.get(CONF_TUYA_VERSION, "auto"),
                             CONF_NODE_ID: cloudList[_devID].get(CONF_NODE_ID, None),
                         }
                     }
