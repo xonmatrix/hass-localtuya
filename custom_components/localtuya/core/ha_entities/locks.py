ca2da1090e31b81d8aa2d26cc437d410fbd86b12
xZetsubou <mrbanderx@gmail.com>
2024-05-12 05:54:20 +0300

    Add auto configure locks

---
+++ custom_components/localtuya/core/ha_entities/locks.py
@@ -0,0 +1,33 @@
+"""
+    This a file contains available tuya data
+    https://developer.tuya.com/en/docs/iot/standarddescription?id=K9i5ql6waswzq
+    Credits: official HA Tuya integration.
+    Modified by: xZetsubou
+"""
+
+from .base import (
+    DPCode,
+    LocalTuyaEntity,
+)
+
+
+def localtuya_lock():
+    """Define localtuya lock configs"""
+    data = {}
+    return data
+
+
+LOCKS: dict[str, tuple[LocalTuyaEntity, ...]] = {
+    # Locks
+    "ms": (
+        LocalTuyaEntity(
+            id=(DPCode.REMOTE_UNLOCK_SWITCH, DPCode.SWITCH),
+            jammed_dp=DPCode.HIJACK,
+            lock_state_dp=(DPCode.OPEN_CLOSE, DPCode.CLOSED_OPENED),
+            custom_configs=localtuya_lock(),
+        ),
+    ),
+}
+
+LOCKS["jtmspro"] = LOCKS["ms"]
+LOCKS["jtmsbh"] = LOCKS["ms"]
